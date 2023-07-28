3ec04a9d40b145213b8aedfcba984bdd09560f29
Kegan Maher <kegan@compiler.la>
2023-07-28 20:11:57 +0000

    fix(test): ensure hashfields is built+published

--- tests/pytest/conftest.py
+++ tests/pytest/conftest.py
@@ -3,9 +3,27 @@ import subprocess
 import pytest
 
 
+@pytest.fixture(scope="session")
+def build_target():
+    return "linux-x64"
+
+
 @pytest.fixture(scope="session", autouse=True)
-def build_hashfields():
+def publish_hashfields(build_target):
     subprocess.call(["dotnet", "build"])
+    subprocess.call(
+        [
+            "dotnet",
+            "publish",
+            "-r",
+            build_target,
+            "-c",
+            "Release",
+            "-o",
+            "./build",
+            "src/Cli/hashfields.csproj",
+        ]
+    )
 
 
 @pytest.fixture
