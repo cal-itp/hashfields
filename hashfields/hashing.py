6a1bb2fab950860562614e6edb909f63240f47b4
Kegan Maher <kegan@compiler.la>
2023-08-17 05:31:20 +0000

    feat(hashfields): CLI supports existing config

---
+++ hashfields/hashing.py
@@ -0,0 +1,2 @@
+ALL_ALGS = ["sha256", "sha384", "sha512"]
+DEFAULT_ALG = "sha512"
--- hashfields/main.py
+++ hashfields/main.py
@@ -1,6 +1,61 @@
-def main():
-    print("hashfields")
+import argparse
+import sys
+
+from hashfields import hashing, __version__ as version
 
 
-if __name__ == "__main__":
-    raise SystemExit(main())
+RESULT_SUCCESS = 0
+
+
+def main(argv=None):
+    argv = argv if argv is not None else sys.argv[1:]
+    parser = argparse.ArgumentParser(prog="hashfields")
+
+    # https://stackoverflow.com/a/8521644/812183
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"%(prog)s {version}",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--alg",
+        choices=hashing.ALL_ALGS,
+        default=hashing.DEFAULT_ALG,
+        dest="hash_alg",
+        help="The hash algorithm to use.",
+    )
+
+    parser.add_argument("-t", "--delimiter", default=",", dest="delimiter", help="Field delimiter in the input data.")
+
+    parser.add_argument(
+        "-d",
+        "--drop",
+        action="extend",
+        default=[],
+        dest="drop",
+        help="Column names to drop from the output.",
+        nargs="+",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-s",
+        "--skip",
+        action="extend",
+        default=[],
+        dest="skip",
+        help="Column names to skip hashing in the output.",
+        nargs="+",
+        type=str,
+    )
+
+    parser.add_argument("-i", "--input", default=sys.stdin, dest="input", help="Readable location for input data.")
+
+    parser.add_argument("-o", "--output", default=sys.stdout, dest="output", help="Writable location for output results.")
+
+    parser.parse_args(argv)
+
+    return RESULT_SUCCESS
